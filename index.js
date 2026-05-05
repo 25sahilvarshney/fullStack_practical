3799b62cd143a807c07487cc92db54d7513fe5f6
SAHIL VARSHNEY <145244634+25sahilvarshney@users.noreply.github.com>
2026-05-05 05:48:26 +0530

    add task routes

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-// DB Connection
+
 connectDB();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/users", authRoutes);
 
-// Test Route
+app.use("/api/users", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
 app.get("/", (req, res) => {
   res.send("Task Manager API Running...");
 });
 
-// Server Start
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
